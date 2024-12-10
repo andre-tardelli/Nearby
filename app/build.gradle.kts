39d87b196c7a8bd774d8a1e411ae0274715478c5
Andre Tardelli <andre.ltardelli@gmail.com>
2024-12-10 20:37:32 +0000

    Primeiro commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.andretardelli.nearby"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.andretardelli.nearby"
--- app/src/main/java/com/andretardelli/nearby/ui/component/NearbyButton.kt
+++ app/src/main/java/com/andretardelli/nearby/ui/component/NearbyButton.kt
@@ -1,2 +1,29 @@
 package com.andretardelli.nearby.ui.component
 
+
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NearbyButton(
+    modifier: Modifier = Modifier,
+    text: String? = null,
+    @DrawableRes inconRes: Int? = null,
+    onClik: () -> Unit
+) {
+    Button(modifier = modifier, onClick = onClik) {
+        text?.let { Text(text = text) }
+    }
+}
+
+@Preview
+@Composable
+private fun NearbyButtonPreview() {
+    NearbyButton (modifier = Modifier.fillMaxWidth(), text = "Confirmar", inconRes = null) {}
+}
